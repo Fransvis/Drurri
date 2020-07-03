4e4cd0dc3115ad27bb89733a17c1d7362b066416
Francois Visser <francoisvisser@Francoiss-Air.Home>
2020-07-03 12:41:34 +0200

    Fransvis: business packages updated for all packages + added freelance package page

--- routes/packages.js
+++ routes/packages.js
@@ -6,12 +6,38 @@ router.get('/', function(req, res){
   res.render('./packages/packages');
 });
 
+// ===============================
+// Business Packages
+// ===============================
+
 router.get('/business', function(req, res){
   res.render('./packages/business');
 });
 
 router.get('/business/pride', function(req, res){
   res.render('./packages/pride');
+});
+
+router.get('/business/gold', function(req, res){
+  res.render('./packages/gold');
+});
+
+router.get('/business/platinum', function(req, res){
+  res.render('./packages/platinum');
+});
+
+router.get('/business/retainer', function(req, res){
+  res.render('./packages/retainer');
+});
+
+
+
+// =====================
+// Freelance package
+// =====================
+
+router.get('/freelance', function(req, res){
+  res.render('./packages/freelance');
 });
 
 module.exports = router;
