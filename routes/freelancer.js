b3420d953f67e3548f1985d1925ce82c856622da
Fransvis <francois.haf@gmail.com>
2020-09-28 13:51:37 +0200

    Fransvis: cleaned freelancer.js route

--- routes/freelancer.js
+++ routes/freelancer.js
@@ -1,14 +1,16 @@
 var express                  = require('express'),
     router                   = express.Router(),
-    // mongoose      = require('mongoose'),
     passportLocalMongoose    = require('passport-local-mongoose'),
     LocalStrategy            = require('passport-local'),
     bodyParser               = require('body-parser'),
     passport                 = require('passport'),
     Project                  = require('../models/projects'),
     FreelanceUser            = require('../models/freelanceUser')
-    // multer                   = require('multer')
 
+
+// ================================
+//          MIDDLEWARE
+// ================================
 
 
 function isLoggedIn(req, res, next){
@@ -18,53 +20,49 @@ function isLoggedIn(req, res, next){
   res.redirect('/login')
 }
 
-// ================================
-// Freelance sign up handle logic
-// ================================
 
-// ==============
-// Sign up page
-// ==============
+// =======================
+//     Sign up page
+// =======================
 
 router.get("/", function(req, res){
   res.render("./freelancer/freelance", {currentUser: req.user});
 });
 
 
-    router.post('/', (req, res) => {
-        const freelancerName    = req.body.Name;
-        const freelancerSurname = req.body.Surname;
-        const username          = req.body.username;
-        // const password          = req.body.password;
+router.post('/', (req, res) => {
+    const freelancerName    = req.body.Name;
+    const freelancerSurname = req.body.Surname;
+    const username          = req.body.username;
       
-        var newFreelanceUser = new FreelanceUser(
-          {
-            freelancerName: freelancerName,
-            freelancerSurname: freelancerSurname,
-            username: username
-          }
-        )
+    var newFreelanceUser = new FreelanceUser(
+      {
+        freelancerName: freelancerName,
+        freelancerSurname: freelancerSurname,
+        username: username
+      }
+    )
       
-        FreelanceUser.register(newFreelanceUser, req.body.password, function(err, newlyCreatedFreelanceUser){
-          if(err){
-            console.log(err);
-            res.render('./packages/freelance');
-          }
-          passport.authenticate('freelancerLocal')(req, res, function() {
-            console.log(freelancerName);
-          });
-          req.login(newFreelanceUser, (err) => {
-            if(err){
-              console.log(err);
-            } else {
-              return res.redirect('freelancer/:id/createprofile')
-            }
-          });
-        });
+    FreelanceUser.register(newFreelanceUser, req.body.password, function(err, newlyCreatedFreelanceUser){
+      if(err){
+        console.log(err);
+        res.render('./packages/freelance');
+      }
+      passport.authenticate('freelancerLocal')(req, res, function() {
+        console.log(freelancerName);
       });
+      req.login(newFreelanceUser, (err) => {
+        if(err){
+          console.log(err);
+        } else {
+          return res.redirect('freelancer/:id/createprofile')
+        }
+      });
+    });
+  });
 
 // ================================
-// add additional information
+//        CREATE PROFILE 
 // ================================
 
 router.get('/:id/createprofile', (req, res) => {
@@ -75,71 +73,55 @@ router.get('/:id/createprofile', (req, res) => {
 
 router.post('/:id/createprofile', (req, res) => {
 
-        var currentUser = req.user;
-        const industry = req.body.industry;
-        const location = req.body.location;
-        const about = req.body.about;
-        const link1 = req.body.link1;
-        const specialty = req.body.specialty;
-        const picture = req.body.picture;
-        const link2 = req.body.link2;
-        const tools = req.body.tools;
+    var currentUser  = req.user;
+    const industry   = req.body.industry;
+    const location   = req.body.location;
+    const about      = req.body.about;
+    const link1      = req.body.link1;
+    const specialty  = req.body.specialty;
+    const picture    = req.body.picture;
+    const link2      = req.body.link2;
+    const tools      = req.body.tools;
 
-        // FreelanceUser.findOneAndUpdate({_id: currentUser._id}, 
-        //   {
-        //     industry: industry,
-        //     location: location
-        //   }, function(err, updatedFreelancer){
-        //     if(err){
-        //       console.log(err)
-        //     } else {
-        //       res.redirect('/')
-        //     }
-        //   })
-
-        FreelanceUser.findOneAndUpdate({_id: currentUser._id}, 
-          {
-            industry: industry, 
-            location: location,
-            about: about,
-            specialty: specialty,
-            picture: picture,
-            link1: link1,
-            link2: link2,
-            tools: tools
-          }, function(err, updatedFreelancer){
-          if(err){
-            console.log(err);
-          } else {
-            res.redirect('/');
-          }
-        });
-
+    FreelanceUser.findOneAndUpdate({_id: currentUser._id}, 
+      {
+        industry: industry, 
+        location: location,
+        about: about,
+        specialty: specialty,
+        picture: picture,
+        link1: link1,
+        link2: link2,
+        tools: tools
+      }, function(err, updatedFreelancer){
+      if(err){
+        console.log(err);
+      } else {
+        res.redirect('/');
+      }
     });
+});
 
-// ======================
-// Display User profile 
-// ======================
+// ==========================
+//    PROFILE SHOW PAGE 
+// ==========================
     
     
-    router.get('/:id/profile', (req, res) => {
-      var currentUser = req.user;
-      // console.log(currentUser);
+router.get('/:id/profile', (req, res) => {
+  var currentUser = req.user;
       
+  FreelanceUser.findById(req.params.id).populate('projects').exec(function(err, foundFreelancer) {
+    if(err){
+      console.log(err);
+    } else {
+      res.render('./freelancer/profile', {freelancer: foundFreelancer, currentUser: req.user});
+    }
+  });
+});
 
-      FreelanceUser.findById(req.params.id).populate('projects').exec(function(err, foundFreelancer) {
-        if(err){
-          console.log(err);
-        } else {
-          // console.log(foundFreelancer)
-          res.render('./freelancer/profile', {freelancer: foundFreelancer, currentUser: req.user});
-        }
-      });
-    });
-
-// ======================
-// Edit User profile 
-// ======================
+// =========================
+//    EDIT USER PROFILE
+// =========================
 
 router.get('/:id/profile/edit', (req, res) => {
 
@@ -153,8 +135,6 @@ router.get('/:id/profile/edit', (req, res) => {
 });
 
 router.put('/:id/profile', (req, res) => {
-// console.log(req.params.id);
-// console.log(req.body.freelancer);
 
    FreelanceUser.findByIdAndUpdate(req.params.id, req.body.freelancer, (err, updatedFreelancer) => {
     if(err) {
@@ -167,85 +147,80 @@ router.put('/:id/profile', (req, res) => {
 });
 
 
-// =====================
-// Add Project to user
-// =====================
+// ===================================
+//   ADD PROJECT TO SPECIFIC USER
+// ===================================
 
-        // go and check for currentUser middleware on app.js (campground auth part 2 vid)
-        // write checkprofileOwnership middleware
-    router.get('/:id/profile/addProject',  (req, res) => {
-      if(req.isAuthenticated()){
-        FreelanceUser.findById(req.params.id, (err, freelancer) => {
-          if(err){
-            console.log(err);
-          } else {
-            if(freelancer._id.equals(req.user._id)) {
-              res.render('./freelancer/addProject', {freelancer: freelancer});
-            } else {
-              res.send("You do not have permission")
-            }
-          }
-        });
-      } else{
-        res.send("You are not authenitcated")
+router.get('/:id/profile/addProject',  (req, res) => {
+  if(req.isAuthenticated()){
+    FreelanceUser.findById(req.params.id, (err, freelancer) => {
+      if(err){
+        console.log(err);
+      } else {
+        if(freelancer._id.equals(req.user._id)) {
+          res.render('./freelancer/addProject', {freelancer: freelancer});
+        } else {
+          res.send("You do not have permission")
+        }
       }
     });
+  } else{
+    res.send("You are not authenitcated")
+  }
+});
 
 
 router.post('/:id/profile/addProject', (req, res) => {      
 
-      FreelanceUser.findById(req.params.id, (err, freelancer) => {
-        if(err){
-          console.log(err);
-          res.redirect('/')
-        } else {
+  FreelanceUser.findById(req.params.id, (err, freelancer) => {
+    if(err){
+      console.log(err);
+      res.redirect('/');
+    } else {
 
-          const title = req.body.title;
-          const date  = req.body.date
-          const image = req.body.image;
-          const newProject = {
-            title: title,
-            date: date,
-            image: image
-          }
+      const title = req.body.title;
+      const date  = req.body.date
+      const image = req.body.image;
+      const newProject = {
+        title: title,
+        date: date,
+        image: image
+      }
 
 
           // Creating duplicate
           
-          Project.create(newProject, (err, project) =>{
-            if(err){
-              console.log(err);
-            } else {
-              freelancer.projects.push(project)
-              freelancer.save();
-              res.redirect('/freelancer/' + freelancer._id + '/profile')
-              console.log(project);
-            }
-          })
-        }
-      });
-
-      
-    });
-
-
-
-// =====================
-// PROJECT SHOW PAGE
-// =====================
-
-    router.get('/:id/profile/showProject', (req, res) => {
-      
-      Project.findById(req.params.id, (err, foundProject) => {
+      Project.create(newProject, (err, project) =>{
         if(err){
           console.log(err);
-          res.redirect('./freelancer/:id/profile')
         } else {
-          // console.log(foundProject)
-          res.render('./freelancer/project', {project: foundProject})
+          freelancer.projects.push(project)
+          freelancer.save();
+          res.redirect('/freelancer/' + freelancer._id + '/profile')
+          console.log(project);
         }
-      })
-    });
+      });
+    }
+  }); 
+});
+
+
+
+// ==========================
+//     PROJECT SHOW PAGE
+// ==========================
+
+router.get('/:id/profile/showProject', (req, res) => {
+      
+  Project.findById(req.params.id, (err, foundProject) => {
+    if(err){
+      console.log(err);
+      res.redirect('./freelancer/:id/profile')
+    } else {
+      res.render('./freelancer/project', {project: foundProject});
+    }
+  });
+});
 
 
 // =====================
